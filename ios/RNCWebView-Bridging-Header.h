4671137d079ea0a8801ead5d69835fd8e6ee80e7
Peter Andringa <peter.j.andringa@gmail.com>
2020-02-23 21:14:58 -0500

    fix bridging header

---
+++ ios/RNCWebView-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RNCWebView-Bridging-Header.h
+//  RNCWebView
+//
+//  Created by Peter Andringa on 2/23/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef RNCWebView_Bridging_Header_h
+#define RNCWebView_Bridging_Header_h
+
+
+#endif /* RNCWebView_Bridging_Header_h */
